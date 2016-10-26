e263ad669fed1f8f24c0e60345acf307f2fbc5a9
jason goodell <jason.goodell@outlook.com>
2016-10-26 13:57:14 -0400

    added data to tables

--- DotnetDB/MonsterDB/Script.PostDeployment.sql
+++ DotnetDB/MonsterDB/Script.PostDeployment.sql
@@ -9,3 +9,23 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]               
 --------------------------------------------------------------------------------------
 */
+use MonsterDB
+go
+
+insert into Monster.Gender(GenderName,Active) values 
+(N'female', 1),
+(N'Male',1),
+(N'Alien',1);
+go
+
+insert into Monster.MonsterType(TypeName,Active) values
+(N'Vampire',1),
+(N'Werewolf',1),
+(N'Zombie',1);
+go
+
+insert into Monster.Title (TitleName,Active) values
+(N'Sir',1),
+(N'Lady',1),
+(N'Count',1);
+go
