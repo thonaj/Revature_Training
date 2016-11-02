1c2be8cebd8e1a778018326b8e4d207f4dd51c36
jason goodell <jason.goodell@outlook.com>
2016-11-02 13:15:57 -0400

    add service to reg

---
+++ RegistrationDAL/RegistrationService/Models/CourseDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationService.Models
+{
+   public class CourseDAO
+   {
+      public int courseID { get; set; }
+      public string courseName { get; set; }
+      public string courseDept { get; set; }
+      public int courseCapacity { get; set; }
+      public string courseProfessor { get; set; }
+      public System.TimeSpan startTime { get; set; }
+      public System.TimeSpan endTime { get; set; }
+      public Nullable<int> currentEnrollment { get; set; }
+      public int courseCredits { get; set; }
+      public bool active { get; set; }
+   }
+}
