bffd325616d86a9872af72f02677d6bbfa70da98
jason goodell <jason.goodell@outlook.com>
2016-11-04 22:01:06 -0400

    copy connectionstring to web config

--- RegistrationDAL/RegistrationDAL/RegistrationDB.Designer.cs
+++ RegistrationDAL/RegistrationDAL/RegistrationDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Revature\Repos\Revature_Training\RegistrationDAL\RegistrationDAL\RegistrationEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\jason\Source\Repos\RegistrationDB\RegistrationDAL\RegistrationDAL\RegistrationDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
