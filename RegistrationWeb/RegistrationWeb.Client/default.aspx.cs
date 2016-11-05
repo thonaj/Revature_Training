c662392bf8542d8fa4c23636c85f08bd5629b02c
jason goodell <jason.goodell@outlook.com>
2016-11-04 20:44:40 -0400

    create default.aspx

---
+++ RegistrationWeb/RegistrationWeb.Client/default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RegistrationWeb.Logic;
+
+
+namespace RegistrationWeb.Client
+{
+   public partial class _default : System.Web.UI.Page
+   {
+
+      protected void Page_Load(object sender, EventArgs e)
+      {
+         getStudents();
+
+      }
+      private void getStudents()
+      {
+         var data = new DataService();
+         studentlist.Items.Clear();
+         foreach (var item in data.getStudents())
+         {
+            studentlist.Items.Add(item.Name);
+         }
+         
+         
+
+      }
+     
+   }
+}
